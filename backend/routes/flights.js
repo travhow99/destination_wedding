97b0485764aedcd7b4369f749864d194af1f7a72
travis <trav.horn@gmail.com>
2020-02-08 12:39:53 -0700

    Add flights full CRUD

--- backend/routes/flights.js
+++ backend/routes/flights.js
@@ -19,4 +19,32 @@ router.route('/add').post((req, res) => {
         .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
+router.route('/:id').get((req, res) => {
+    Flight.findById(req.params.id)
+        .then((flight) => res.json(flight))
+        .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+router.route('/:id').delete((req, res) => {
+    Flight.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Flight deleted.'))
+        .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+router.route('update/:id').post((req, res) => {
+    Flight.findById(req.params.id)
+        .then((flight) => {
+            flight.flight_number = req.body.flight_number;
+            flight.airline = req.body.airline;
+            flight.departure_city = req.body.departure_city;
+            flight.arrival_city = req.body.arrival_city;
+
+            flight.save()
+                .then(() => res.json('Flight updated!'))
+                .catch((err) => res.status(400).json(`Error: ${err}`));
+        })
+        .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+
 module.exports = router;
