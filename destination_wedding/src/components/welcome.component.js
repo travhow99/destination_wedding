a283e42b4f26246e40ffb87b1334a6cf2af0f9c8
travis <trav.horn@gmail.com>
2020-02-12 21:01:53 -0700

    Adding cookies for initial auth and user storage

--- destination_wedding/src/components/welcome.component.js
+++ destination_wedding/src/components/welcome.component.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import logo from '../mountains.png';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
 
 export default class Welcome extends Component {
     constructor(props) {
@@ -11,7 +14,22 @@ export default class Welcome extends Component {
         this.state = {
             entry_key: 'fake_entry',
             user_entry: '',
+            attendee: false,
+            name: '',
+            name_input: '',
         }
+    }
+
+    componentDidMount() {
+        console.log(this.state);
+
+        if (cookies.get('attendee')) {
+            // alert("Welcome!");
+            this.setState({
+                attendee: true,
+            })
+        }
+
     }
 
     onChangeInput(e) {
@@ -20,14 +38,38 @@ export default class Welcome extends Component {
         })
     }
 
+    onChangeName(e) {
+        this.setState({
+            name_input: e.target.value,
+        })
+    }
+
     onSubmit(e) {
         e.preventDefault();
 
         if (this.state.user_entry === this.state.entry_key) {
-            alert('WELCOME!');
+            var expires = new Date();
+            expires.setDate(expires.getDate() + 7);
+
+            cookies.set('attendee', true, { 
+                path: '/',
+                expires: expires,
+            });
+
+            this.setState({
+                attendee: true,
+            });
         } else {
             // Show Error Message
         }
+    }
+
+    onSubmitName(e) {
+        e.preventDefault();
+
+        this.setState({
+            name: this.state.name_input,
+        })
     }
 
     render() {
@@ -37,19 +79,33 @@ export default class Welcome extends Component {
                 <h1>Welcome!</h1>
                 <p>Enter the sign in code below:</p>
 
-                <form onSubmit={this.onSubmit}>
-
-                    <div className="form-group">
-                        <input 
-                            onChange={this.onChangeInput} 
-                            className="" />
-                    </div>
-
-                    <div className="form-group">
-                        <button type="submit" className="btn btn-primary">Submit</button>
-                    </div>
-
-                </form>
+                { !this.state.attendee ? (
+                    <form onSubmit={this.onSubmit}>
+                        <div className="form-group">
+                            <input 
+                                onChange={this.onChangeInput} 
+                                className="" />
+                        </div>
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                    ) : (
+                    <form onSubmit={this.onSubmitName}>
+                        <div className="form-group">
+                            
+                            <label>Your Name:</label>
+                            
+                            <input 
+                                onChange={this.onChangeName} 
+                                className="" />
+                        </div>
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-primary">test</button>
+                        </div>
+                    </form>
+                    )
+                }
 
             </div>
         )
